e551a666da4c3da4e47dd4941b16ca004bdae545
carlos silva <betinho.vicosa@gmail.com>
2021-09-01 13:46:14 -0300

    initial

--- DataDogApiSample/Controllers/WeatherForecastController.cs
+++ DataDogApiSample/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace DataDogApiSample.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation($"get ok");
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
@@ -35,5 +36,52 @@ namespace DataDogApiSample.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet("GetRioForecast")]
+        public IEnumerable<WeatherForecast> GetRioForecast()
+        {
+            _logger.LogInformation($"get ok");
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[99]
+            })
+            .ToArray();
+        }
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Post()
+        {
+            _logger.LogInformation($"post ok");
+            var rng = new Random();
+            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpDelete]
+        public void Delete()
+        {
+            for (int i = 1; i < 20; i++)
+            {
+                try
+                {
+                    _logger.LogInformation($"Loop {i}");
+                    throw new Exception($"Erro - {i}");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message);
+                }    
+            }
+            
+        }
+
+
     }
 }
